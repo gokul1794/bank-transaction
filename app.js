97c5070261fc9daba4c3407043371a1b1039f5f0
R Gokul Shanth <gokul.shanth@gmail.com>
2018-03-30 21:24:43 +0400

    Avoid callback hell by breaking the code to functions of queries.

--- app.js
+++ app.js
@@ -14,21 +14,6 @@ app.use(validator());
 
 app.get('/',(req, res) => res.send('Hello world! My first Node app.'))
 
-app.get('/transactions1', (req, res) => {
-	//Connect to DB Done
-	//Handle transfer two times
-	//Handle database unavailability (With a transactional rollback) Done
-	//TWo people transfer to the same third person(Try with a test case?)
-	//Validate request data Done
-	//Use async await Done
-	//console.log("Request "+JSON.stringify(req.body));	
-	checkifAccountExists(req.body.from)
-	getBalances().then(result =>{
-		res.json(result);
-	});
-	
-})
-
 app.post('/transactions', asyncMiddleware( async(req,res,next)=>{
 	req.checkBody("from", "Enter a valid email address.").isEmail().exists();
 	req.checkBody("to","Enter a valid email address.").isEmail().exists();
@@ -61,22 +46,44 @@ app.post('/transactions', asyncMiddleware( async(req,res,next)=>{
 	}
 }));
 
+/* Updated balances and Inserts into the transaction table */
 function updateBalance(request){
 	return new Promise(async function(resolve,reject){
-		 try{
-		 pool.getConnection(function(err, connection) {
-		 	connection.beginTransaction(function(err){
-		 		if (err) {          
-		 		console.log("Couldn't begin transaction");        //Transaction Error (Rollback and release connection)
-            	connection.rollback(function() {
-                connection.release();
-                reject(err);
-                //Failure
-            		});
-        		} 
-        		let result2,result3;
-		 		let result1 = connection.query("UPDATE balances SET balances.balance = balances.balance-"
-		 		+request.amount+" where balances.accountNumber = '"+request.from+"'",function(error,result1){
+		try{
+			pool.getConnection(function(err,connection){
+				connection.beginTransaction(function(err){
+					if(err){
+						console.log("Couldn't begin transaction");        //Transaction Error (Rollback and release connection)
+            			connection.rollback(function() {
+                		connection.release();
+                		reject(err);
+                		//Failure
+            			});
+					}
+					updateFromAccount(request,connection)
+					.then(updateToAccount(request,connection))
+					.then(insertIntoTransaction(request,connection).then(result=>{
+						getTransaction(result,connection).then(response=>{
+							resolve(response);
+						});
+					})).catch(function (err) { // <- See this <<<<<<<<
+        				return res.send(err);
+    				});
+				});
+			});
+		}catch(err){
+			reject(err);
+		}
+	});
+}
+
+
+/* Updates From account's balance */
+function updateFromAccount(request,connection){
+	return new Promise(async function(resolve,reject){
+		try{
+			connection.query("UPDATE balances SET balances.balance = balances.balance-"
+		 		+request.amount+" where balances.accountNumber = '"+request.from+"'",function(error,result){
 		 			if(error){
 		 				console.log("Error while updating to balances table fromAccount;Roll back");
 		 				return connection.rollback(function() {
@@ -84,9 +91,19 @@ function updateBalance(request){
         					reject(err);
       						});
 		 				}
-		 				console.log(JSON.stringify(result1));
-		 		result2 = connection.query("UPDATE balances SET balances.balance = balances.balance+"
-		 		+request.amount+" where balances.accountNumber = '"+request.to+"'",function(error,result2){
+		 	});
+		}catch(err){
+			reject(err);
+		}
+	});
+}
+
+/* Updates to account's balance */
+function updateToAccount(request,connection){
+	return new Promise(async function(resolve,reject){
+		try{
+				connection.query("UPDATE balances SET balances.balance = balances.balance+"
+		 		+request.amount+" where balances.accountNumber = '"+request.to+"'",function(error,result){
 		 			if(error){
 		 				console.log("Error while updating to balances table for toAccount;Roll back");
 		 				return connection.rollback(function() {
@@ -94,9 +111,19 @@ function updateBalance(request){
         					reject(err);
       						});
 		 				}
-		 				console.log(JSON.stringify(result2));
-		 		const referenceNo = uuidv1();
-		 		result3 = connection.query("insert into transactions (transactionRef,amount,fromAccount,toAccount,transactionDate) values('"+referenceNo+"',"+request.amount+",'"+request.from+"','"+request.to+"',CURRENT_TIMESTAMP());",function(error,result3){
+		 			});
+		}catch(err){
+			reject(err);
+		}
+	});
+}
+
+/* Inserts into the transaction table */
+function insertIntoTransaction(request,connection){
+	return new Promise(async function(resolve,reject){
+		try{
+			const referenceNo = uuidv1();
+			result3 = connection.query("insert into transactions (transactionRef,amount,fromAccount,toAccount,transactionDate) values('"+referenceNo+"',"+request.amount+",'"+request.from+"','"+request.to+"',CURRENT_TIMESTAMP());",function(error,result3){
 		 			if(error){
 		 				console.log("Error while inserting to Transaction table;Roll back");
 		 				return connection.rollback(function() {
@@ -104,8 +131,7 @@ function updateBalance(request){
         					reject(err);
       						});
 		 				}
-		 				console.log(JSON.stringify(result3));
-		 		connection.commit(function(err) {
+		 				connection.commit(function(err) {
         		   if (err) {
         		   	console.log("Couldn't commit transaction;Roll back");
           			return connection.rollback(function() {
@@ -113,8 +139,21 @@ function updateBalance(request){
             		  reject(err);
           			 });
         			}
-        			console.log('success!');
-        			let response = connection.query("select * from transactions where transactions.transactionRef='"+referenceNo+"';", function(error,result,fields){
+		 		})
+		 				resolve(referenceNo);
+		 			});
+
+		}catch(err){
+			reject(err);
+		}
+	});
+}
+
+/* Get transaction by transactionId */
+function getTransaction(transactionId,connection){
+	return new Promise(async function(resolve,reject){
+		try{
+			let response = connection.query("select * from transactions where transactions.transactionRef='"+transactionId+"';", function(error,result,fields){
         				//console.log(response);
         				if(error){
         					console.log("Transaction saved but couldn't fetch details.");
@@ -123,23 +162,13 @@ function updateBalance(request){
         				console.log(JSON.stringify(result));
         				resolve(result);
         			});
-      			});
-		 				
-		 	});
-		 				
-		 });
-		});
-	   });
-	});	
-	}catch(err){
-		console.log("Error");
-		console.log(err);
-		throw new Error(err);
-		reject(err);
+		}catch(err){
+			reject(err);
 		}
 	});
 }
 
+/* Check if account exists with balance */
 function checkifAccountExists(id,amount){
 	return new Promise(async function(resolve,reject){
 		 try{
