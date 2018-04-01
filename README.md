f367000789ee442d7e2db206bf043433eb7b7a82
R Gokul Shanth <gokul.shanth@gmail.com>
2018-04-02 01:05:25 +0400

    added tests and transfer twice logic.

--- README.md
+++ README.md
@@ -1,10 +1,34 @@
-# bank-transaction
-Transfer money from A to B
-SQL Tables
+# Bank Transaction Problem
+Before running the app, make sure you have
+1. Nodejs
+2. Mysql
+
+`git clone` and do `npm install` in the directory.
+
+Edit the `database.js` file to add your db details.
+
+Make sure mysql is running.
+
+`cd test` and run `npm test` to run all the tests.
+
+1. I have assumed that, a user cannot transfer the same amount to the same person in less than x seconds which is configured in `app.js` for the variable `transferTwiceTimeOut`
+2. If database becomes unavailable in the middle of my logic, transaction fails and it rolls back.
+3. If two people A and B transfer at the same time, there won't be any issues with concurrency as I believe Mysql is capable of handling ACID transactions and puts in appropriate locks to handle it.
+4. Data is validated when the request is made, for simplicity I've assumed that from and to are emails.
+5. Used async await.
+
+Use These SQL commands to set the db up quickly if you'd like to test.
+
 ```
 CREATE TABLE balances (accountNumber VARCHAR(20) NOT NULL UNIQUE, balance DECIMAL(13,4));
-CREATE TABLE transactions(transactionRef varchar(40) NOT NULL UNIQUE, amount DECIMAL(13,4) NOT NULL, 
-fromAccount VARCHAR(20) NOT NULL, toAccount VARCHAR(20) NOT NULL, transactionDate DATETIME);```
+```
 
-For sake of simplicity, It is assumed that the from and to account is an email.
+```
+CREATE TABLE transactions(transactionRef varchar(40) NOT NULL UNIQUE, amount DECIMAL(13,4) NOT NULL, fromAccount VARCHAR(20) NOT NULL, toAccount VARCHAR(20) NOT NULL, transactionDate DATETIME);
+```
 
+```
+insert into balances values('gokul@gmail.com',500);
+insert into balances values('abhi@gmail.com',1000);
+insert into balances values('avesh@gmail.com',750);
+```
--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var pool = require('./database')
 var asyncMiddleware = require('./asyncMiddleware');
 const uuidv1 = require('uuid/v1');
 
+const transferTwiceTimeOut = 10;
+const SUCCESS = "success";
+const FAILURE = "failure";
+
 var validator = require('express-validator');
 
 const app = express()
@@ -12,19 +16,22 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
 app.use(validator());
 
-app.get('/',(req, res) => res.send('Hello world! My first Node app.'))
+app.get('/',(req, res) => res.send({'message':'Hello world! My first Node app.'}) );
 
 app.post('/transactions', asyncMiddleware( async(req,res,next)=>{
+	console.log("Transaction time "+ new Date().toString());
 	req.checkBody("from", "Enter a valid email address.").isEmail().exists();
 	req.checkBody("to","Enter a valid email address.").isEmail().exists();
 	req.checkBody("amount","Amount should be a number").isDecimal().exists();
 	var errors = req.validationErrors();
 	if (errors) {
-    res.send({ "errors" : errors});
+    res.send({ 
+    	"status":FAILURE, 
+    	"errors" : errors
+    	});
     return;
   	} 
-	// const user = await getBalances().then(alert);
-	if(req.body.amount>1){
+	if(req.body.amount>1&&req.body.from!==req.body.to){
 	var checkifFromExistsWithBalance = checkifAccountExists(req.body.from,req.body.amount);
 	var checkifToAccountExists = checkifAccountExists(req.body.to,0);
 	Promise.all([checkifFromExistsWithBalance,checkifToAccountExists]).then(function(values){
@@ -32,19 +39,58 @@ app.post('/transactions', asyncMiddleware( async(req,res,next)=>{
 					updateBalance(req.body).then(result=>{
 						res.send(result);
 					}).catch(err=>{
-						res.send({"errors" :err});
+						res.send(err);
 					})
 		}else{
-			res.send("check account numbers and balances");	
+			res.send({"status": FAILURE, 
+				"errors" : "check account numbers and balances"
+			});	
 		}
 	}).catch(err => {
     console.error(err);
-    res.send({"errors" :err}); 
+    res.send({"status": FAILURE, 
+    			"errors" :err
+    			}); 
   });
 	}else{
-		res.send({ "error ": "Amount value not set right"})
+		res.send({ "status": FAILURE,
+			 		"errors": "check request body again"
+				});
 	}
 }));
+
+/* Checking if similar transactions exist*/
+function checkSimilarTransaction(request){
+	return new Promise(async function(resolve,reject){
+		try{
+			let result = await pool.query("select * from transactions where fromAccount ='"+
+		 request.from+"' and toAccount = '"+request.to+
+		 "'and amount = "+request.amount+
+		 " order by transactionDate desc limit 1;");
+		if(result[0]){
+			let timeDifference = ((new Date())-result[0].transactionDate)/1000;
+			if(timeDifference<transferTwiceTimeOut){
+				resolve({"status":FAILURE, 
+					"data": "Possible duplicate transaction "+timeDifference.toString()});
+			}
+			else{
+				resolve({"status":SUCCESS,
+						 "data": "New transaction, Old transaction was "+timeDifference.toString()+" seconds ago."
+							});
+			}
+		}
+		else{
+			resolve({"status" : SUCCESS ,
+						 "data" : "No record found in the DB, new transaction."
+						});
+			}
+		}catch(err){
+			reject({"status" : FAILURE ,
+					 "errors" : err
+					});
+		}
+	});
+}
 
 /* Updated balances and Inserts into the transaction table */
 function updateBalance(request){
@@ -56,23 +102,41 @@ function updateBalance(request){
 						console.log("Couldn't begin transaction");        //Transaction Error (Rollback and release connection)
             			connection.rollback(function() {
                 		connection.release();
-                		reject(err);
+                		reject({"status":FAILURE, "errors":err});
                 		//Failure
             			});
 					}
-					updateFromAccount(request,connection)
-					.then(updateToAccount(request,connection))
-					.then(insertIntoTransaction(request,connection).then(result=>{
-						getTransaction(result,connection).then(response=>{
-							resolve(response);
-						});
-					})).catch(function (err) { // <- See this <<<<<<<<
-        				reject(err)
-    				});
+					checkSimilarTransaction(request)
+					.then(result=>{
+						if(result.status===SUCCESS){
+							updateFromAccount(request,connection)
+								.then(updateToAccount(request,connection))
+								.then(insertIntoTransaction(request,connection)
+								.then(result=>{
+									getTransaction(result,connection)
+									.then(response=>{
+										resolve({"status":SUCCESS,
+													"data" : response
+													});
+										});
+									})).catch(function (err) {
+        							reject({"status":FAILURE,
+        									 "errors":err})
+    							});
+							}else{
+								reject({"status":FAILURE, 
+										"errors":"Possible duplicate transaction"
+										})
+							}
+					}).catch(function (err) {
+        							reject({"status":FAILURE,
+        									 "errors":err})
+    							});
 				});
 			});
 		}catch(err){
-			reject(err);
+			reject({"status":FAILURE,
+					 "errors":err});
 		}
 	});
 }
@@ -88,12 +152,14 @@ function updateFromAccount(request,connection){
 		 				console.log("Error while updating to balances table fromAccount;Roll back");
 		 				return connection.rollback(function() {
         					//throw error;
-        					reject(err);
+        					reject({"status":FAILURE,
+        							 "errors":err});
       						});
 		 				}
 		 	});
 		}catch(err){
-			reject(err);
+			reject({"status":FAILURE,
+					 "errors":err});
 		}
 	});
 }
@@ -108,12 +174,14 @@ function updateToAccount(request,connection){
 		 				console.log("Error while updating to balances table for toAccount;Roll back");
 		 				return connection.rollback(function() {
         					//throw error;
-        					reject(err);
+        					reject({"status":FAILURE,
+        						 "errors":err});
       						});
 		 				}
 		 			});
 		}catch(err){
-			reject(err);
+			reject({"status":FAILURE,
+					 "errors":err});
 		}
 	});
 }
@@ -123,12 +191,15 @@ function insertIntoTransaction(request,connection){
 	return new Promise(async function(resolve,reject){
 		try{
 			const referenceNo = uuidv1();
-			result3 = connection.query("insert into transactions (transactionRef,amount,fromAccount,toAccount,transactionDate) values('"+referenceNo+"',"+request.amount+",'"+request.from+"','"+request.to+"',CURRENT_TIMESTAMP());",function(error,result3){
+			result3 = connection.query("insert into transactions (transactionRef,amount,fromAccount,toAccount,transactionDate) values('"
+				+referenceNo+"',"+request.amount+",'"+request.from+"','"+request.to
+				+"',CURRENT_TIMESTAMP());",function(error,result3){
 		 			if(error){
 		 				console.log("Error while inserting to Transaction table;Roll back");
 		 				return connection.rollback(function() {
         					//throw error;
-        					reject(err);
+        					reject({"status":FAILURE,
+        							 "errors":err});
       						});
 		 				}
 		 				connection.commit(function(err) {
@@ -136,15 +207,16 @@ function insertIntoTransaction(request,connection){
         		   	console.log("Couldn't commit transaction;Roll back");
           			return connection.rollback(function() {
             		  //throw err;
-            		  reject(err);
+            		  reject({"status":FAILURE,
+            		  			 "errors":err});
           			 });
         			}
-		 		})
+		 		});
 		 				resolve(referenceNo);
 		 			});
 
 		}catch(err){
-			reject(err);
+			reject({"status":FAILURE, "errors":err});
 		}
 	});
 }
@@ -153,7 +225,8 @@ function insertIntoTransaction(request,connection){
 function getTransaction(transactionId,connection){
 	return new Promise(async function(resolve,reject){
 		try{
-			let response = connection.query("select * from transactions where transactions.transactionRef='"+transactionId+"';", function(error,result,fields){
+			let response = connection.query("select * from transactions where transactions.transactionRef='"
+				+transactionId+"';", function(error,result,fields){
         				//console.log(response);
         				if(error){
         					console.log("Transaction saved but couldn't fetch details.");
@@ -163,7 +236,8 @@ function getTransaction(transactionId,connection){
         				resolve(result);
         			});
 		}catch(err){
-			reject(err);
+			reject({"status":FAILURE,
+					 "errors":err});
 		}
 	});
 }
@@ -172,11 +246,13 @@ function getTransaction(transactionId,connection){
 function checkifAccountExists(id,amount){
 	return new Promise(async function(resolve,reject){
 		 try{
-		 	let result = await pool.query("SELECT * FROM balances WHERE balances.accountNumber = '"+id+"' and balances.balance>="+amount+" LIMIT 1");
+		 	let result = await pool.query("SELECT * FROM balances WHERE balances.accountNumber = '"
+		 		+id+"' and balances.balance>="+amount+" LIMIT 1");
 		 	//console.log("Result "+JSON.stringify(result[0]));
 		 	resolve(result[0]);
 		 }catch(err){
-		 	reject(err);
+		 	reject({"status":FAILURE,
+		 			 "errors":err});
 		 }
 	});
 }
@@ -186,11 +262,23 @@ function getBalances(){
 		 try{
 		 	let result = await pool.query('select * from balances');
 		 	//console.log("Result "+JSON.stringify(result[0]));
-		 	resolve(result[0]);
+		 	resolve(result);
 		 }catch(err){
-		 	reject(err);
+		 	reject({"status":FAILURE,
+		 			 "errors":err});
 		 }
 	});
 } 
+
+module.exports = {
+	getBalances: getBalances,
+	checkifAccountExists: checkifAccountExists,
+	getTransaction: getTransaction,
+	insertIntoTransaction: insertIntoTransaction,
+	updateToAccount: updateToAccount,
+	updateFromAccount: updateFromAccount,
+	updateBalance: updateBalance,
+	checkSimilarTransaction: checkSimilarTransaction
+}
 
 app.listen(3000, () => console.log('Bank transaction example.'))
